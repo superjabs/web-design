c67e391398c0164f7b659f34691f7a36541bcffd
alfian <gusjabungaf@gmail.com>
2023-06-02 22:20:42 +0700

    css style

--- pendidikan.php
+++ pendidikan.php
@@ -5,25 +5,24 @@
 ?>
 
 <!-- Pendidikan -->
-
 <article>
   <h2>Pendidikan Saya</h2>
+  <section class='timeline-section'>
+    <div class='timeline-items'>
   <?php 
       $sql = "SELECT * FROM pendidikan";
       $result = $conn->query($sql);
 
       while($data = $result->fetch_assoc()){
         echo "
-        <section class='timeline-section'>
-        <div class='timeline-items'>
           <div class='timeline-item'>
             <div class='timeline-dot'></div>
             <div class='timeline-title'> " . $data['jenjang'] . "  (" .  $data['tahun'] . ")</div>
             <div class='timeline-degrees'> " . $data['jurusan'] . " </div>
           </div>
-        </div>
-      </section>
-        ";
-      }
-  ?>
+          ";
+        }
+        ?>
+      </div>
+    </section>
 </article>
